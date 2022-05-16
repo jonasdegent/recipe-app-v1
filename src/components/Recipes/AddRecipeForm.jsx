e92f078d64156c33777d128b202d2972797b7acb
jonasdegent <71662016+jonasdegent@users.noreply.github.com>
2022-05-16 21:56:10 +0200

    added allergens

--- src/components/Recipes/AddRecipeForm.jsx
+++ src/components/Recipes/AddRecipeForm.jsx
@@ -11,23 +11,32 @@ import CircularProgress from '@mui/material/CircularProgress';
 import MenuItem from '@mui/material/MenuItem';
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Chip from "@mui/material/Chip";
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import { Card } from '@mui/material';
 
 const defaultValues = {
   title: "",
   subtitle: "",
   timing: "",
-  category: "Hoofdgerecht"
+  category: "Hoofdgerecht",
+  allergens: []
 }
 
 const AddRecipeForm = () => {
   const { documents : recipes } = useCollection('recipes')
+  const { documents : allergens } = useCollection('allergens')
 
   const [formValues, setFormValues] = useState(defaultValues)
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormValues({
-      ...formValues, [name]: value
+      ...formValues, [name]: typeof value === "string" ? value.split(",") : value
+      
     })
   }
 
@@ -52,52 +61,69 @@ const AddRecipeForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Grid>
-        <TextField
-          label="Naam gerecht" 
-          variant="standard" 
-          size="small"
-          name="title"
-          value={formValues.title}
-          onChange={handleInputChange}
-        />
-        <TextField 
-          label="Korte beschrijving" 
-          variant="standard" 
-          size="small"
-          name="subtitle"
-          value={formValues.subtitle}
-          onChange={handleInputChange}
-        />
-        <TextField 
-          label="Kooktijd"
-          variant="standard" 
-          size="small"
-          name="timing"
-          value={formValues.timing}
-          onChange={handleInputChange}
-        />
-      </Grid>
-      <Grid>
-        <TextField 
-          label="Categorie" 
-          select
-          name="category"
-          value={formValues.category}
-          onChange={handleInputChange}
-          variant="standard" 
-          size="small"
-        >
-          { [...undupedCategories].map((category) => (
-            <MenuItem key={category} value={category}>
-              {category}
-            </MenuItem>
-          ))}
-        </TextField>
-      </Grid>
+    <FormControl sx={{marginLeft: 3}}>
+      <TextField
+        label="Naam gerecht" 
+        variant="standard" 
+        name="title"
+        value={formValues.title}
+        onChange={handleInputChange}
+      />
+      <TextField 
+        label="Korte beschrijving" 
+        variant="standard" 
+        name="subtitle"
+        value={formValues.subtitle}
+        onChange={handleInputChange}
+      />
+      <TextField 
+        label="Kooktijd"
+        variant="standard" 
+        name="timing"
+        value={formValues.timing}
+        onChange={handleInputChange}
+      />
+      <TextField 
+        label="Categorie" 
+        select
+        name="category"
+        value={formValues.category}
+        onChange={handleInputChange}
+        variant="standard" 
+      >
+        { [...undupedCategories].map((category) => (
+          <MenuItem key={category} value={category}>
+            {category}
+          </MenuItem>
+        ))}
+      </TextField>  
+      <Select
+        multiple
+        value={formValues.allergens}
+        onChange={handleInputChange}
+        name="allergens"
+        input={<OutlinedInput label='Allergenen' />}
+        renderValue={(selected) => (
+          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+            {selected.map((value) => (
+              <Chip key={value} label={value} />
+            ))}
+          </Box>
+        )}
+      >
+        {allergens.map((allergen) => (
+          <MenuItem
+            key={allergen.id}
+            value={allergen.name}
+          >
+            {allergen.name}
+          </MenuItem>
+        ))}
+      </Select>    
       <Button variant="contained" color="primary" type="submit">
-          Voeg recept toe
+        Voeg recept toe
       </Button>
+    </FormControl>
     </form>
   )
 }
